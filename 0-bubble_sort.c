445002477bfe2f9befe9154f897bf1de6e7fef94
Yos <99474058+Yosinan@users.noreply.github.com>
2023-01-22 17:15:42 +0300

    Update 0-bubble_sort.c

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -4,6 +4,7 @@
  * swap_ints - Swap two integers in an array.
  * @a: The first integer to swap.
  * @b: The second integer to swap.
+ 
  */
 void swap_ints(int *a, int *b)
 {
@@ -24,21 +25,21 @@ void swap_ints(int *a, int *b)
 void bubble_sort(int *array, size_t size)
 {
 	size_t i, len = size;
-	bool bubbly = false;
+	bool bubbl = false;
 
 	if (array == NULL || size < 2)
 		return;
 
-	while (bubbly == false)
+	while (bubbl == false)
 	{
-		bubbly = true;
+		bubbl = true;
 		for (i = 0; i < len - 1; i++)
 		{
 			if (array[i] > array[i + 1])
 			{
 				swap_ints(array + i, array + i + 1);
 				print_array(array, size);
-				bubbly = false;
+				bubbl = false;
 			}
 		}
 		len--;
